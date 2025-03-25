01bd0da7230a9116031a69d21bf0fdd95c887986
ruan-cat <1219043956@qq.com>
2025-03-25 13:00:18 +0800

    🐳 chore: 保存，正在迁移写法。

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -227,9 +227,6 @@ const handleLogout = () => {
 	logoutDialog.value.showDialog();
 };
 
-// 未读公告标题列表
-const unreadNoticeList = ref([]);
-
 // TODO 去往公告详情页
 const goNoticeDetail = async (type: string, id: string) => {
 	// 修改成已读
@@ -237,36 +234,12 @@ const goNoticeDetail = async (type: string, id: string) => {
 	await updateNoticeStatusAPI({
 		noticeId: id,
 	});
-	getUnreadNoticeList();
+	// TODO: 待迁移
+	// getUnreadNoticeList();
 	noticeId.value = id;
 	dialogTitle.value = type === "1" ? "通知公告详情" : "系统消息详情";
 	showDetail.value = true;
 };
-
-// 未读公告数量
-const unreadNoticeCount = ref(0);
-// 获取未读公告标题列表
-const getUnreadNoticeList = async () => {
-	const res = await getUnreadNoticeListAPI();
-	console.log(res);
-	if (res.code === 10000) {
-		unreadNoticeCount.value = res.data.length;
-		if (res.data.length > 10) {
-			// 最多显示10条未读公告
-			unreadNoticeList.value = res.data.slice(0, 10);
-		} else {
-			unreadNoticeList.value = res.data;
-		}
-	} else if (res.code === 9999) {
-		// 说明没有未读公告
-	} else {
-		ElMessage.error(res.message);
-	}
-};
-
-onMounted(() => {
-	getUnreadNoticeList();
-});
 </script>
 
 <template>
@@ -340,77 +313,9 @@ onMounted(() => {
 				<el-header>
 					<div class="header-nav">
 						<div class="left"><span style="font-size: 20px">WMS仓储管理系统</span></div>
-						<div class="right">
-							<el-dropdown trigger="click">
-								<div class="icon-container">
-									<el-icon :size="20" class="icon move">
-										<Bell />
-										<span class="badge">{{ unreadNoticeCount }}</span>
-									</el-icon>
-								</div>
 
-								<template #dropdown>
-									<el-dropdown-menu size="large">
-										<div class="notice">
-											<div class="header">
-												<el-icon style="margin: 0 4px 0 4px; color: #b471a0" size="16"><WarnTriangleFilled /></el-icon>
-												<span>{{ unreadNoticeCount }}公告</span>
-											</div>
-											<div class="content">
-												<ul>
-													<li
-														v-for="(item, index) in unreadNoticeList"
-														@click="goNoticeDetail('1', item.noticeId)"
-														:key="item.noticeId"
-													>
-														<svg
-															t="1741875609474"
-															class="icon"
-															viewBox="0 0 1107 1024"
-															version="1.1"
-															xmlns="http://www.w3.org/2000/svg"
-															p-id="3048"
-															width="12"
-															height="12"
-														>
-															<path
-																d="M563.323289 661.336738a330.663562 330.663562 0 1 0-330.531877-330.663562 331.190306 331.190306 0 0 0 330.531877 330.663562z"
-																fill="#ffffff"
-																p-id="3049"
-															></path>
-															<path
-																d="M1106.528066 975.53955c-16.724123-134.319727-137.611877-293.001365-273.38015-373.066457a381.889419 381.889419 0 0 1-539.912627-0.65843C157.335331 679.246035 18.274908 834.635523 0.365611 975.53955a42.797952 42.797952 0 0 0 37.26714 48.065392h1031.628175A43.061324 43.061324 0 0 0 1106.528066 975.53955z"
-																fill="#ffffff"
-																p-id="3050"
-															></path>
-														</svg>
-														<span>{{ item.noticeTitle }}</span>
-													</li>
-												</ul>
-											</div>
-											<div class="footer" @click="showDialog(1)">
-												<span>查看全部</span>
-												<svg
-													t="1741346481645"
-													class="icon"
-													viewBox="0 0 1024 1024"
-													version="1.1"
-													xmlns="http://www.w3.org/2000/svg"
-													p-id="1825"
-													width="16"
-													height="16"
-												>
-													<path
-														d="M885.113 489.373L628.338 232.599c-12.496-12.497-32.758-12.497-45.254 0-12.497 12.497-12.497 32.758 0 45.255l203.3 203.3H158.025c-17.036 0-30.846 13.811-30.846 30.846 0 17.036 13.811 30.846 30.846 30.846h628.36L583.084 746.147c-12.497 12.496-12.497 32.758 0 45.255 6.248 6.248 14.438 9.372 22.627 9.372s16.379-3.124 22.627-9.372l256.775-256.775a31.999 31.999 0 0 0 0-45.254z"
-														fill="#4f99c6"
-														p-id="1826"
-													></path>
-												</svg>
-											</div>
-										</div>
-									</el-dropdown-menu>
-								</template>
-							</el-dropdown>
+						<div class="right">
+							<ComponentsHeaderRightNotice />
 
 							<el-dropdown trigger="click">
 								<div class="icon-container">
@@ -564,7 +469,7 @@ onMounted(() => {
 	<noticeDetail v-model:control="showDetail" :dialogTitle="dialogTitle" :noticeId="noticeId"></noticeDetail>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .sec-container {
 	height: 100vh;
 }
@@ -636,7 +541,7 @@ onMounted(() => {
 	outline: unset;
 }
 
-/* 按钮小数字 */
+/* 按钮小数字 TODO: 待迁移 */
 .icon-container {
 	position: relative;
 	display: inline-block;
@@ -650,17 +555,17 @@ onMounted(() => {
 
 .badge {
 	position: absolute;
-	top: 0px; /* 距离图标上边的距离 */
-	right: 0px; /* 距离图标右边的距离 */
-	background-color: red; /* 背景色，可以修改 */
-	color: white; /* 字体颜色 */
-	width: 10px; /* 宽度 */
-	height: 10px; /* 高度 */
-	border-radius: 50%; /* 圆形 */
+	top: 0px;
+	right: 0px;
+	background-color: red;
+	color: white;
+	width: 10px;
+	height: 10px;
+	border-radius: 50%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	font-size: 12px; /* 数字字体大小 */
+	font-size: 12px;
 }
 
 .move {

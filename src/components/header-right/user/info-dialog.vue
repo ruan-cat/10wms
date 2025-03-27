1570fa0e3a334600a97da9a2a4d4980f54769443
ruan-cat <1219043956@qq.com>
2025-03-27 13:54:03 +0800

    🐳 chore: 正在开发用户详情弹框。

--- src/components/header-right/user/info-dialog.vue
+++ src/components/header-right/user/info-dialog.vue
@@ -3,26 +3,63 @@
 	显示用户信息，可修改用户信息
 -->
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, useTemplateRef } from "vue";
 import { type DialogPromiseProps } from "components/dialog-promise/types";
+import { type BaseFormProps } from "components/base-form/index.vue";
 
-const dialogPromiseProps = ref<DialogPromiseProps<null>>({
+/** 弹框组件配置 */
+const dialogPromiseProps = ref<DialogPromiseProps<{}>>({
 	async onDialogClose({ reject, resolve }) {
-		resolve(null);
+		resolve({});
 		return true;
 	},
 });
 
-const form = ref({
+/** 弹框组件实例 */
+const dialogRef = useTemplateRef("dialogRef");
+
+/**
+ * 用户信息
+ * @description
+ * TODO: 临时写的 不清楚后端是那个接口
+ */
+interface UserInfo {
+	username: string;
+	name: string;
+	phone: string;
+	email: string;
+}
+
+const form = ref<UserInfo>({
 	username: "admin",
 	name: "管理员",
 	phone: "",
 	email: "",
 });
 
+/** 表单配置 */
+const baseFormProps = ref<BaseFormProps<UserInfo>>({
+	form: form.value,
+	formItems: [
+		{
+			type: "input",
+			props: {
+				label: "年龄",
+				prop: "age",
+				placeholder: "请输入年龄",
+				maxlength: 10,
+				responsive: { md: 12, lg: 8 },
+			},
+		},
+	],
+});
+
+/** 表单组件实例 */
+const baseFormRef = useTemplateRef("baseFormRef");
+
 const handleSubmit = () => {
 	// 这里可以添加保存逻辑
-	alert("个人信息已保存！");
+	// alert("个人信息已保存！");
 	// handleClose();
 };
 
@@ -32,13 +69,16 @@ const handleUpdateVisible = (value: boolean) => {
 </script>
 
 <template>
-	<el-dialog
-		:model-value="visible"
-		title="个人信息"
-		width="30%"
-		:before-close="handleClose"
-		@update:model-value="handleUpdateVisible"
-	>
+	<ComponentsDialogPromise :="dialogPromiseProps" ref="dialogRef">
+		<template #default>
+			<ComponentsBaseForm :="baseFormProps" ref="baseFormRef"></ComponentsBaseForm>
+		</template>
+
+		<template #footer="{ reject, resolve }">
+			<ElButton type="info" @click="dialogPromiseProps.onDialogClose({ reject, resolve })"> 关闭 </ElButton>
+		</template>
+	</ComponentsDialogPromise>
+	<el-dialog title="个人信息" width="30%" :before-close="handleClose" @update:model-value="handleUpdateVisible">
 		<el-form :model="form" label-width="100px">
 			<el-form-item label="用户账号">
 				<el-input v-model="form.username" disabled />

05b532edc1ad32af9bf8b36492dc8cf6bf490990
ruan-cat <1219043956@qq.com>
2025-03-28 13:52:10 +0800

    ✨ feat(通用基础表单): 直接绑定传递下来的数据。

--- src/components/base-form/index.vue
+++ src/components/base-form/index.vue
@@ -209,12 +209,7 @@ defineExpose({
 				>
 					<ElFormItem :label="formItem.props.label" :prop="formItem.props.prop">
 						<!-- 输入框 -->
-						<ElInput
-							v-if="formItem.type === 'input'"
-							v-model="form[formItem.props.prop]"
-							:placeholder="formItem.props.placeholder"
-							:maxlength="formItem.props.maxlength"
-						/>
+						<ElInput v-if="formItem.type === 'input'" v-model="form[formItem.props.prop]" :="formItem" />
 
 						<!-- 选择器 -->
 						<ElSelect v-else-if="formItem.type === 'select'" v-model="form[formItem.props.prop]">

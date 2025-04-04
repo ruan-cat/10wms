945ab5963465f751940f0f4badbed99f7c78f86c
ruan-cat <1219043956@qq.com>
2025-04-04 14:21:01 +0800

    ✨ feat: 重做分页栏组件。

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -1,95 +1,77 @@
-<template>
-	<div class="pagination">
-		<el-pagination
-			v-model:current-page="currentPages"
-			background
-			:layout="layout"
-			:pager-count="pagerCount"
-			:page-sizes="pageSizes"
-			:page-size="pageSize"
-			:total="pageTotal"
-			@size-change="sizeChange"
-			@current-change="current"
-			@prev-click="prev"
-			@next-click="next"
-		></el-pagination>
-	</div>
-</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+import type { PaginationProps as ElPaginationProps } from "element-plus";
 
-<script setup>
-import { ref, reactive } from "vue";
-const props = defineProps({
-	pageTotal: {
-		type: Number,
-		default: 0, //总页数
-	},
-	pagerCount: {
-		type: Number,
-		default: 5, //如果页数很多大概展示的页码
-	},
-	layout: {
-		type: String,
-		default: "total,sizes, prev, pager, next, jumper, ->, slot", //分页组件会展示的功能项
-	},
-	pageSizes: {
-		type: Array,
-		default: () => {
-			return [10]; //指定分页展示条数
-		},
-	},
-	currentPage: {
-		type: Number,
-		default: 1, //指定跳转到多少页
-	},
-	pageSize: {
-		type: Number,
-		default: 10, //每页展示的条数，根据自己实际，且会带入请求
-	},
-	pageNum: {
-		type: Number,
-		default: 1, //第几页数据，根据自己实际，且会带入请求
-	},
+/** 预设的分页栏属性 */
+type PresetPaginationProps = Partial<ElPaginationProps>;
+
+/**
+ * 预设的分页栏属性
+ * @description
+ * 分页栏就非固定封装一些属性 其他的不提供
+ */
+const paginationProps = ref<PresetPaginationProps>({
+	layout: "total, sizes, prev, pager, next, jumper, ->, slot",
+	pageSizes: [10, 15, 30, 50, 100],
+	background: true,
 });
 
-let currentPages = ref(props.currentPage);
-let pageData = reactive({
-	pageSize: props.pageSize,
-	pageNum: props.pageNum,
+/** 分页栏属性 */
+export interface PaginationProps extends PresetPaginationProps {
+	/**
+	 * 总页数
+	 * @description
+	 * 对应的分页接口必须传递总页数
+	 */
+	total: number;
+
+	/**
+	 * 异步请求函数
+	 * @description
+	 * 分页栏组件会自动监听内部数据变化 同时做出相应的请求
+	 */
+	asyncFunc: (...args: any[]) => Promise<any>;
+}
+
+/** 对外暴露需要设置值的 props */
+const props = defineProps<PaginationProps>();
+
+/** 给分页栏组件批量赋值的 props 对象 */
+const toPaginationProps = computed(() => {
+	return {
+		...paginationProps.value,
+		...props,
+	};
 });
 
-const emit = defineEmits(["pageFunc"]);
+/**
+ * 当前页码
+ * @description
+ * 即 PageDTO 的 `pageIndex`
+ */
+const pageIndex = defineModel<number>();
 
-//选择每页显示数量 Change page size
-const sizeChange = (val) => {
-	pageData.pageSize = val;
-	emit("pageFunc", pageData);
-};
-//选择某一页
-const current = (val) => {
-	pageData.pageNum = currentPages.value;
-	emit("pageFunc", pageData);
-};
-//上一页
-const prev = () => {
-	pageData.pageNum = pageData.pageNum - 1;
-	emit("pageFunc", pageData);
-};
-//下一页
-const next = () => {
-	pageData.pageNum = pageData.pageNum + 1;
-	emit("pageFunc", pageData);
-};
+/**
+ * 每页显示最大数据条数
+ * @description
+ * 即 PageDTO 的 `pageSize`
+ */
+const pageSize = defineModel<number>();
 </script>
 
-<style scoped lang="less">
-.pagination {
-	width: 100%;
-	background: #fff;
-	padding: 5px 0;
-	text-align: center;
-	margin: 0 auto;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-}
+<template>
+	<ElPagination :="toPaginationProps" v-model:current-page="pageIndex" v-model:page-size="pageSize"></ElPagination>
+</template>
+
+<style lang="scss" scoped>
+// .pagination {
+// 	width: 100%;
+// 	background: #fff;
+// 	padding: 5px 0;
+// 	text-align: center;
+// 	margin: 0 auto;
+// 	display: flex;
+// 	flex-direction: column;
+// 	align-items: center;
+// }
 </style>

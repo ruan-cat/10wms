70e0e89d031701fc8de5a2435acfac9c22c91343
ruan-cat <1219043956@qq.com>
2025-05-15 19:29:52 +0800

    🐞 fix(router,main): 处理类型报错。

--- main/src/router/index.ts
+++ main/src/router/index.ts
@@ -133,52 +133,98 @@ const whiteList = ["/login"];
 
 const { VITE_HIDE_HOME } = import.meta.env;
 
-router.beforeEach((to: ToRouteType, _from, next) => {
-	if (to.meta?.keepAlive) {
-		handleAliveRoute(to, "add");
-		// 页面整体刷新和点击标签页刷新
-		if (_from.name === undefined || _from.name === "Redirect") {
-			handleAliveRoute(to);
-		}
-	}
-	const userInfo = storageLocal().getItem<DataInfo<number>>(userKey);
-	NProgress.start();
-	const externalLink = isUrl(to?.name as string);
-	if (!externalLink) {
-		to.matched.some((item) => {
-			if (!item.meta.title) return "";
-			const Title = getConfig().Title;
-			if (Title) document.title = `${transformI18n(item.meta.title)} | ${Title}`;
-			else document.title = transformI18n(item.meta.title);
-		});
-	}
-	/** 如果已经登录并存在登录信息后不能跳转到路由白名单，而是继续保持在当前页面 */
-	function toCorrectRoute() {
-		whiteList.includes(to.fullPath) ? next(_from.fullPath) : next();
-	}
-	if (Cookies.get(multipleTabsKey) && userInfo) {
-		// 无权限跳转403页面
-		if (to.meta?.roles && !isOneOfArray(to.meta?.roles, userInfo?.roles)) {
-			next({ path: "/error/403" });
-		}
-		// 开启隐藏首页后在浏览器地址栏手动输入首页welcome路由则跳转到404页面
-		if (VITE_HIDE_HOME === "true" && to.fullPath === "/welcome") {
-			next({ path: "/error/404" });
-		}
-		if (_from?.name) {
-			// name为超链接
-			if (externalLink) {
-				openLink(to?.name as string);
-				NProgress.done();
-			} else {
-				toCorrectRoute();
+router.beforeEach(
+	(
+		/**
+		 * to:ToRouteType
+		 * 不手动增加类型了 目前使用了自动化路由 增加了很多类型
+		 * 该类型会导致很多报错 故不增加冗余的类型 使其自动推断即可
+		 */
+		to,
+		_from,
+		next,
+	) => {
+		if (to.meta?.keepAlive) {
+			handleAliveRoute(to, "add");
+			// 页面整体刷新和点击标签页刷新
+			if (_from.name === undefined || _from.name === "Redirect") {
+				handleAliveRoute(to);
 			}
-		} else {
-			// 刷新
-			if (usePermissionStoreHook().wholeMenus.length === 0 && to.path !== "/login") {
-				function oldInitRouter() {
-					initRouter().then((router: Router) => {
+		}
+		const userInfo = storageLocal().getItem<DataInfo<number>>(userKey);
+		NProgress.start();
+		const externalLink = isUrl(to?.name as string);
+		if (!externalLink) {
+			to.matched.some((item) => {
+				if (!item.meta.title) return "";
+				const Title = getConfig().Title;
+				if (Title) document.title = `${transformI18n(item.meta.title)} | ${Title}`;
+				else document.title = transformI18n(item.meta.title);
+			});
+		}
+		/** 如果已经登录并存在登录信息后不能跳转到路由白名单，而是继续保持在当前页面 */
+		function toCorrectRoute() {
+			whiteList.includes(to.fullPath) ? next(_from.fullPath) : next();
+		}
+		if (Cookies.get(multipleTabsKey) && userInfo) {
+			// 无权限跳转403页面
+			if (to.meta?.roles && !isOneOfArray(to.meta?.roles, userInfo?.roles)) {
+				next({ path: "/error/403" });
+			}
+			// 开启隐藏首页后在浏览器地址栏手动输入首页welcome路由则跳转到404页面
+			if (VITE_HIDE_HOME === "true" && to.fullPath === "/welcome") {
+				next({ path: "/error/404" });
+			}
+			if (_from?.name) {
+				// name为超链接
+				if (externalLink) {
+					openLink(to?.name as string);
+					NProgress.done();
+				} else {
+					toCorrectRoute();
+				}
+			} else {
+				// 刷新
+				if (usePermissionStoreHook().wholeMenus.length === 0 && to.path !== "/login") {
+					function oldInitRouter() {
+						initRouter().then((router: Router) => {
+							if (!useMultiTagsStoreHook().getMultiTagsCache) {
+								const { path } = to;
+								const route = findRouteByPath(path, router.options.routes[0].children);
+								getTopMenu(true);
+								// query、params模式路由传参数的标签页不在此处处理
+								if (route && route.meta?.title) {
+									if (isAllEmpty(route.parentId) && route.meta?.backstage) {
+										// 此处为动态顶级路由（目录）
+										const { path, name, meta } = route.children[0];
+										useMultiTagsStoreHook().handleTags("push", {
+											path,
+											name,
+											meta,
+										});
+									} else {
+										const { path, name, meta } = route;
+										useMultiTagsStoreHook().handleTags("push", {
+											path,
+											name,
+											meta,
+										});
+									}
+								}
+							}
+							// 确保动态路由完全加入路由列表并且不影响静态路由（注意：动态路由刷新时router.beforeEach可能会触发两次，第一次触发动态路由还未完全添加，第二次动态路由才完全添加到路由列表，如果需要在router.beforeEach做一些判断可以在to.name存在的条件下去判断，这样就只会触发一次）
+							if (isAllEmpty(to.name)) router.push(to.fullPath);
+						});
+					}
+
+					/** @see https://pure-admin.cn/pages/routerMenu/#如何只要静态路由 */
+					function newInitRouter() {
+						// 使用下面方法替换initRouter
+						usePermissionStoreHook().handleWholeMenus([]);
+						addPathMatch();
 						if (!useMultiTagsStoreHook().getMultiTagsCache) {
+							console.log("newInitRouter to", to);
+
 							const { path } = to;
 							const route = findRouteByPath(path, router.options.routes[0].children);
 							getTopMenu(true);
@@ -204,60 +250,25 @@ router.beforeEach((to: ToRouteType, _from, next) => {
 						}
 						// 确保动态路由完全加入路由列表并且不影响静态路由（注意：动态路由刷新时router.beforeEach可能会触发两次，第一次触发动态路由还未完全添加，第二次动态路由才完全添加到路由列表，如果需要在router.beforeEach做一些判断可以在to.name存在的条件下去判断，这样就只会触发一次）
 						if (isAllEmpty(to.name)) router.push(to.fullPath);
-					});
-				}
-
-				/** @see https://pure-admin.cn/pages/routerMenu/#如何只要静态路由 */
-				function newInitRouter() {
-					// 使用下面方法替换initRouter
-					usePermissionStoreHook().handleWholeMenus([]);
-					addPathMatch();
-					if (!useMultiTagsStoreHook().getMultiTagsCache) {
-						console.log("newInitRouter to", to);
-
-						const { path } = to;
-						const route = findRouteByPath(path, router.options.routes[0].children);
-						getTopMenu(true);
-						// query、params模式路由传参数的标签页不在此处处理
-						if (route && route.meta?.title) {
-							if (isAllEmpty(route.parentId) && route.meta?.backstage) {
-								// 此处为动态顶级路由（目录）
-								const { path, name, meta } = route.children[0];
-								useMultiTagsStoreHook().handleTags("push", {
-									path,
-									name,
-									meta,
-								});
-							} else {
-								const { path, name, meta } = route;
-								useMultiTagsStoreHook().handleTags("push", {
-									path,
-									name,
-									meta,
-								});
-							}
-						}
 					}
-					// 确保动态路由完全加入路由列表并且不影响静态路由（注意：动态路由刷新时router.beforeEach可能会触发两次，第一次触发动态路由还未完全添加，第二次动态路由才完全添加到路由列表，如果需要在router.beforeEach做一些判断可以在to.name存在的条件下去判断，这样就只会触发一次）
-					if (isAllEmpty(to.name)) router.push(to.fullPath);
+					newInitRouter();
 				}
-				newInitRouter();
-			}
-			toCorrectRoute();
-		}
-	} else {
-		if (to.path !== "/login") {
-			if (whiteList.indexOf(to.path) !== -1) {
-				next();
-			} else {
-				removeToken();
-				next({ path: "/login" });
+				toCorrectRoute();
 			}
 		} else {
-			next();
+			if (to.path !== "/login") {
+				if (whiteList.indexOf(to.path) !== -1) {
+					next();
+				} else {
+					removeToken();
+					next({ path: "/login" });
+				}
+			} else {
+				next();
+			}
 		}
-	}
-});
+	},
+);
 
 router.afterEach(() => {
 	NProgress.done();

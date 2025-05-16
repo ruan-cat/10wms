a337eed2c04be5ca1c4241c3b6d0b629ca5d07c1
ruan-cat <1219043956@qq.com>
2025-05-16 14:35:07 +0800

    📃 docs(vite,wms-frontend-origin): 更新issue链接

--- origin/vite.config.ts
+++ origin/vite.config.ts
@@ -177,7 +177,7 @@ export default defineConfig(({ mode }) => {
 			 * @description
 			 * vueDevTools 必须在 createHtmlPlugin 的前面导入
 			 *
-			 * @see https://github.com/vuejs/devtools-next/issues/278#issuecomment-2021745201
+			 * @see https://github.com/vuejs/devtools/issues/278#issuecomment-2167415057
 			 */
 			vueDevTools(),
 
